23d362b7f19cee2b6d3797d7ebdf107d0c9d5530
Shaivi <shaivisri04@gmail.com>
2025-12-25 19:06:52 +0530

    Create attrition_by_manager_support.sql

---
+++ sql/attrition_by_manager_support.sql
@@ -0,0 +1,15 @@
+SELECT
+  CASE
+    WHEN avg_manager_support_score < 3 THEN 'Low Manager Support'
+    WHEN avg_manager_support_score < 4 THEN 'Medium Manager Support'
+    ELSE 'High Manager Support'
+  END AS manager_support_bucket,
+
+  COUNT(*) AS total_employees,
+
+  SUM(CASE WHEN attrition_flag = TRUE THEN 1 ELSE 0 END) AS attrited_employees,
+
+  SAFE_DIVIDE(
+    SUM(CASE WHEN attrition_flag = TRUE THEN 1 ELSE 0 END),
+    COUNT(*)
+  ) AS attrition_rate
