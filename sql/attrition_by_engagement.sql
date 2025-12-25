8457ba3a941beac76aef09cc31a5c1aeef709272
Shaivi <shaivisri04@gmail.com>
2025-12-25 19:04:47 +0530

    Create attrition_by_engagement.sql

---
+++ sql/attrition_by_engagement.sql
@@ -0,0 +1,19 @@
+SELECT
+  CASE
+    WHEN avg_engagement_score < 3 THEN 'Low Engagement'
+    WHEN avg_engagement_score < 4 THEN 'Medium Engagement'
+    ELSE 'High Engagement'
+  END AS engagement_bucket,
+
+  COUNT(*) AS total_employees,
+
+  SUM(CASE WHEN attrition_flag = TRUE THEN 1 ELSE 0 END) AS attrited_employees,
+
+  SAFE_DIVIDE(
+    SUM(CASE WHEN attrition_flag = TRUE THEN 1 ELSE 0 END),
+    COUNT(*)
+  ) AS attrition_rate
+
+FROM `level-footing-481113-s9.hr_analytics.employee_base`
+GROUP BY engagement_bucket
+ORDER BY attrition_rate DESC
